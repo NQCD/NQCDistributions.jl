fbcd612e4286cd4769ce3863f8a8a3a558358ffb
James Gardner <james.gardner1421@gmail.com>
2022-04-28 13:15:04 +0100

    Files generated by PkgTemplates

---
+++ src/NQCDistributions.jl
@@ -0,0 +1,5 @@
+module NQCDistributions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NQCDistributions
+using Test
+
+@testset "NQCDistributions.jl" begin
+    # Write your tests here.
+end

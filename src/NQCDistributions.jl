c30d260f1fc2dd4c2d7d72bf75f6685ee4f3416a
James Gardner <james.gardner1421@gmail.com>
2022-04-28 15:22:54 +0100

    Initial commit

--- src/NQCDistributions.jl
+++ src/NQCDistributions.jl
@@ -1,5 +1,135 @@
 module NQCDistributions
 
-# Write your package code here.
+using Distributions: Sampleable, Univariate
+using Random: Random, AbstractRNG, SamplerTrivial
+using RingPolymerArrays: RingPolymerArray, eachbead
+
+export UnivariateFill
+export UnivariateArray
+export FixedArray
+export FixedValue
+export ConfigurationVector
+export RingPolymerWrapper
+
+"""
+    UnivariateFill{S<:Sampleable{Univariate}}
+
+Fill all degrees of freedom from single Univariate distribution.
+"""
+struct UnivariateFill{S<:Sampleable{Univariate}}
+    sampleable::S
+    dims::Dims{2}
+end
+function Random.rand(rng::AbstractRNG, d::SamplerTrivial{<:UnivariateFill})
+    return rand(rng, d[].sampleable, d[].dims)
+end
+function Random.rand!(rng::AbstractRNG, a::AbstractMatrix, d::SamplerTrivial{<:UnivariateFill})
+    return Random.rand!(rng, d[].sampleable, a)
+end
+Base.eltype(d::UnivariateFill) = Matrix{eltype(d.sampleable)}
+Base.size(d::UnivariateFill) = d.dims
+
+"""
+    UnivariateArray{S<:Sampleable{Univariate}}
+
+Fill each degree of freedom from a different Univariate distribution.
+
+The size of the matrix of sampleables should match the system size.
+"""
+struct UnivariateArray{S<:Sampleable{Univariate}}
+    sampleable::Matrix{S}
+end
+function Random.rand(rng::AbstractRNG, d::SamplerTrivial{<:UnivariateArray})
+    out = similar(d[].sampleable, eltype(eltype(d[].sampleable)))
+    return Random.rand!(rng, out, d)
+end
+function Random.rand!(rng::AbstractRNG, a::AbstractMatrix, d::SamplerTrivial{<:UnivariateArray})
+    for I in eachindex(a, d[].sampleable)
+        a[I] = rand(rng, d[].sampleable[I])
+    end
+    return a
+end
+Base.eltype(d::UnivariateArray) = Matrix{eltype(eltype(d.sampleable))}
+Base.size(d::UnivariateArray) = size(d.sampleable)
+
+"""
+    FixedArray{S<:AbstractArray}
+
+Return the same configuration every time.
+"""
+struct FixedArray{S<:AbstractArray}
+    value::S
+end
+function Random.rand(::AbstractRNG, d::SamplerTrivial{<:FixedArray})
+    return copy(d[].value)
+end
+function Random.rand!(::AbstractRNG, a::AbstractArray, d::SamplerTrivial{<:FixedArray})
+    copy!(a, d[].value)
+    return a
+end
+Base.eltype(d::FixedArray) = typeof(d.value)
+Base.size(d::FixedArray) = size(d.value)
+
+"""
+    FixedValue{S<:Real}
+
+Fill all degrees of freedom with the same value every time.
+"""
+struct FixedValue{S<:Real}
+    value::S
+    dims::Dims{2}
+end
+function Random.rand(::AbstractRNG, d::SamplerTrivial{<:FixedValue})
+    return fill(d[].value, d[].dims)
+end
+function Random.rand!(::AbstractRNG, a::AbstractMatrix, d::SamplerTrivial{<:FixedValue})
+    fill!(a, d[].value)
+    return a
+end
+Base.eltype(d::FixedValue) = Matrix{typeof(d.value)}
+Base.size(d::FixedValue) = d.dims
+
+"""
+    ConfigurationVector{S<:AbstractVector}
+
+Sample from a provided vector of configurations.
+"""
+struct ConfigurationVector{S<:AbstractVector}
+    configurations::S
+end
+function Random.rand(rng::AbstractRNG, d::SamplerTrivial{<:ConfigurationVector})
+    return copy(rand(rng, d[].configurations))
+end
+function Random.rand!(rng::AbstractRNG, a::AbstractArray, d::SamplerTrivial{<:ConfigurationVector})
+    copy!(a, rand(rng, d[].configurations))
+    return a
+end
+Base.eltype(d::ConfigurationVector) = eltype(d.configurations)
+Base.size(d::ConfigurationVector) = size(first(d.configurations))
+
+"""
+    RingPolymerWrapper{S}
+
+Wrap other distributions to convert them to ring polymer distributions.
+"""
+struct RingPolymerWrapper{S}
+    sampleable::S
+    dims::Dims{3}
+    classical::Vector{Int}
+end
+function RingPolymerWrapper(sampleable, nbeads; classical=Int[])
+    return RingPolymerWrapper(sampleable, (size(sampleable)..., nbeads), classical)
+end
+function Random.rand(rng::AbstractRNG, d::SamplerTrivial{<:RingPolymerWrapper})
+    out = RingPolymerArray{eltype(eltype(d[].sampleable))}(undef, d[].dims; d[].classical)
+    Random.rand!(rng, out, d)
+    return out
+end
+function Random.rand!(rng::AbstractRNG, a::RingPolymerArray, d::SamplerTrivial{<:RingPolymerWrapper})
+    for bead in eachbead(a)
+        Random.rand!(rng, bead, d[].sampleable)
+    end
+    return a
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,103 @@
 using NQCDistributions
 using Test
 
-@testset "NQCDistributions.jl" begin
-    # Write your tests here.
+using Distributions: Normal
+using Random: rand!
+using RingPolymerArrays: RingPolymerArray, eachbead
+
+@testset "UnivariateFill" begin
+    d = UnivariateFill(Normal(), (3,2))
+    @test eltype(d) == Matrix{Float64}
+    @test rand(d) isa AbstractMatrix
+    out = zeros(3,2)
+    rand!(out, d)
+    @test all(out .!== 0.0)
+end
+
+@testset "UnivariateArray" begin
+    d = UnivariateArray([Normal() for i=1:3, j=1:2])
+    @test eltype(d) == Matrix{Float64}
+    @test rand(d) isa AbstractMatrix
+    out = zeros(3,2)
+    rand!(out, d)
+    @test all(out .!== 0.0)
+end
+
+@testset "FixedArray" begin
+    d = FixedArray([1 2; 3 4; 5 6])
+    @test eltype(d) == Matrix{Int}
+    @test rand(d) isa AbstractMatrix
+    out = zeros(3,2)
+    rand!(out, d)
+    @test out == [1.0 2.0; 3.0 4.0; 5.0 6.0]
+
+    a = rand(3,2,3)
+    d = FixedArray(a)
+    @test eltype(d) == Array{Float64,3}
+    @test rand(d) isa AbstractArray{Float64,3}
+    out = zeros(3,2,3)
+    rand!(out, d)
+    @test out == a
+end
+
+@testset "FixedValue" begin
+    d = FixedValue(1.0, (3,2))
+    @test eltype(d) == Matrix{Float64}
+    @test rand(d) isa AbstractMatrix
+    out = zeros(3,2)
+    rand!(out, d)
+    @test out == ones(3,2)
+end
+
+@testset "ConfigurationVector" begin
+    d = ConfigurationVector([ones(3,2) for _=1:3])
+    @test eltype(d) == Matrix{Float64}
+    @test rand(d) isa AbstractMatrix
+    out = zeros(3,2)
+    rand!(out, d)
+    @test out == ones(3,2)
+
+    d = ConfigurationVector([ones(3,2,3) for _=1:3])
+    @test eltype(d) == Array{Float64,3}
+    @test rand(d) isa AbstractArray{Float64,3}
+    out = zeros(3,2,3)
+    rand!(out, d)
+    @test out == ones(3,2,3)
+end
+
+@testset "RingPolymerWrapper" begin
+    nbeads = 4
+    classical = [1]
+
+    @testset "UnivariateFill" begin
+        dsingle = UnivariateFill(Normal(), (3,2))
+        d = RingPolymerWrapper(dsingle, nbeads; classical)
+        @test rand(d) isa RingPolymerArray
+    end
+
+    @testset "UnivariateArray" begin
+        dsingle = UnivariateArray([Normal() for i=1:3, j=1:2])
+        d = RingPolymerWrapper(dsingle, nbeads; classical)
+        @test rand(d) isa RingPolymerArray
+    end
+
+    @testset "FixedArray" begin
+        dsingle = UnivariateArray([Normal() for i=1:3, j=1:2])
+        d = RingPolymerWrapper(dsingle, nbeads; classical)
+        @test rand(d) isa RingPolymerArray
+    end
+
+    @testset "FixedValue" begin
+        dsingle = FixedValue(1.0, (3,2))
+        d = RingPolymerWrapper(dsingle, nbeads; classical)
+        @test rand(d) isa RingPolymerArray
+        @test rand(d) == ones(3,2,4)
+    end
+
+    @testset "ConfigurationVector" begin
+        dsingle = ConfigurationVector([ones(3,2) for _=1:3])
+        d = RingPolymerWrapper(dsingle, nbeads; classical)
+        @test rand(d) isa RingPolymerArray
+        @test rand(d) == ones(3,2,4)
+    end
 end

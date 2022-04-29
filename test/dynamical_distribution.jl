ac916c20f71b87055d15434f893a9aa7f923fcbd
James Gardner <james.gardner1421@gmail.com>
2022-04-29 15:39:53 +0100

    Add tests

--- test/dynamical_distribution.jl
+++ test/dynamical_distribution.jl
@@ -1,9 +1,28 @@
 using NQCDistributions
 using Distributions: Normal
 
-@testset "DynamicalDistribution" begin
+@testset "iterate" begin
     d = DynamicalDistribution(Normal(), 2.0, (3,2))
-    @test d[1] == d[1]
-    @test rand(d) != rand(d)
     @test length(collect(d)) == 1
+
+    d = DynamicalDistribution([Normal() for i=1:3, j=1:2], [rand(3,2) for _=1:10], (3,2))
+    @test length(collect(d)) == 10
+end
+
+@testset "rand" begin
+    d = DynamicalDistribution(Normal(), 2.0, (3,2))
+    @test rand(d) != rand(d)
+    @test rand(d) != rand(d)
+end
+
+@testset "getindex" begin
+    d = DynamicalDistribution([Normal() for i=1:3, j=1:2], Normal(), (3,2))
+    @test d[1] == d[1]
+    @test d[10] == d[10]
+    @test d[100] == d[100]
+
+    d = DynamicalDistribution([Normal() for i=1:3, j=1:2], [rand(3,2) for _=1:10], (3,2))
+    @test d[1] == d[1]
+    @test d[10] == d[10]
+    @test_throws BoundsError d[100] == d[100]
 end

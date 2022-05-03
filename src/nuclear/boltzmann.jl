45894572583c26bf2b297b8366dfc1304bc1d4df
James Gardner <james.gardner1421@gmail.com>
2022-05-03 14:31:40 +0100

    Fix VelocityBoltzmann distribution

--- src/nuclear/boltzmann.jl
+++ src/nuclear/boltzmann.jl
@@ -2,4 +2,4 @@
 function VelocityBoltzmann(temperature, masses::AbstractVector, dims::Dims{2})
     return UnivariateArray([VelocityBoltzmann(temperature, masses[I[2]]) for I in CartesianIndices(dims)])
 end
-VelocityBoltzmann(temperature, mass) = Normal(sqrt(austrip(temperature)/mass))
+VelocityBoltzmann(temperature, mass) = Normal(0, sqrt(austrip(temperature)/mass))

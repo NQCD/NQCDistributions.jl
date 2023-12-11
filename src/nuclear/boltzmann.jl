9e2497c441e98f5b2e41998936a8e376e3e2721c
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2023-12-11 11:25:52 +0000

    Overload Base.:+ for shifting VelocityBoltzmann after initialisation.

--- src/nuclear/boltzmann.jl
+++ src/nuclear/boltzmann.jl
@@ -15,3 +15,24 @@ function VelocityBoltzmann(temperature, masses::AbstractVector, dims::Dims{2};  
     return UnivariateArray([VelocityBoltzmann(temperature, masses[I[2]]; centre = centre[I]) for I in CartesianIndices(dims)])
 end
 VelocityBoltzmann(temperature, mass; centre = 0) = Normal(centre, sqrt(austrip(temperature)/mass))
+
+"""
+    +(dist::VelocityBoltzmann, translation::AbstractArray)
+
+Add a permanent translation to a Velocity distribution, e.g. for a centre of mass translation component. 
+"""
+function Base.:+(dist::UnivariateArray, translation::AbstractArray)
+    if size(dist)!=size(translation)
+        throw(DimensionMismatch("Distribution and translation array must have the same size."))
+    end
+    return UnivariateArray([Normal(dist.sampleable[I].μ+translation[I], dist.sampleable[I].σ) for I in CartesianIndices(dist.sampleable)])
+end
+
+"""
+    +(dist::Normal, translation::Number)
+
+Add a permanent translation to a Velocity distribution. 
+"""
+function Base.:+(dist::Normal, translation::Number)
+    return Normal(dist.μ+convert(Float64, translation), dist.σ)
+end

a786465db090ed1e757967cdbc45ed5fb9e0f461
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2024-04-22 10:46:36 +0100

    Update UnivariateArray to allow for multiple distribution types (#12)
    * Update UnivariateArray to allow for multiple distribution types
    
    * Update gitignore in case something installs Conda.jl
    
    * Broader type constraints for VelocityBoltzmann
    
    ---------
    
    Co-authored-by: Reinhard Maurer <r.maurer@warwick.ac.uk>

--- src/nuclear/boltzmann.jl
+++ src/nuclear/boltzmann.jl
@@ -12,13 +12,13 @@ Generate a Boltzmann distribution of velocities for each degree of freedom.
 * `center` - Vector of dimension ndofs that provides centre of mass velocity offset
 """
 
-function VelocityBoltzmann(temperature, masses::AbstractVector, dims::Dims{2};  center::Array{Float64} = zeros(dims))
+function VelocityBoltzmann(temperature::Number, masses::AbstractVector{<:Number}, dims::Dims{2};  center::AbstractMatrix{<:Number} = zeros(dims))
     return UnivariateArray([VelocityBoltzmann(temperature, masses[I[2]]; center = center[I]) for I in CartesianIndices(dims)])
 end
     
 """
     VelocityBoltzmann(temperature, mass; center = 0)
 """
-function VelocityBoltzmann(temperature::Number, mass::Float64; center::Float64 = 0)
+function VelocityBoltzmann(temperature::Number, mass::Number; center::Number = 0)
     return Normal(center, sqrt(austrip(temperature)/mass))
 end

314fa0dd16deb8227b11ae5cf0fe30b215b05b12
mlarkin863 <mlarkin863@gmail.com>
2024-11-01 18:23:11 +0000

    bug fixes

--- src/electronic/electronic.jl
+++ src/electronic/electronic.jl
@@ -78,7 +78,7 @@ struct NonEqState{S,T,A} <: ElectronicDistribution{S}
     statetype::S # usually the `adiabatic()` or `diabatic()`` labels
     available_states::A # need this, in general will default to `Colon()` as with above.
 end
-function NonEqState(state_vector, statetype=Adiabatic(), available_states=Colon())
-    return NonEqState(state_vector, statetype, available_states)
+function NonEqState(occupation_vector, statetype=Adiabatic(), available_states=Colon())
+    return NonEqState(occupation_vector, statetype, available_states)
 end
 # ------------------------------------------------------------------------------------------------ #
